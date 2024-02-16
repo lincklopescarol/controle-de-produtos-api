624c3c60c37f50a70603974688a394085bd00722
lincklopescarol <lincklopescarol@gmail.com>
2024-02-16 19:24:51 -0300

    feat: adicionando data annotations

--- ControleDeProdutos/Migrations/20240216221432_AjusteTabelas.Designer.cs
+++ ControleDeProdutos/Migrations/20240216221432_AjusteTabelas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ControleDeProdutos.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ControleDeProdutos.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240216221432_AjusteTabelas")]
+    partial class AjusteTabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
