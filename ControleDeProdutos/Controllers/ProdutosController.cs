879dc77e0ced7727fc2311d8c37b12a603e2d79b
lincklopescarol <lincklopescarol@gmail.com>
2024-02-16 22:44:34 -0300

    feat: adicionando ProdutosController

---
+++ ControleDeProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeProdutos.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeProdutos.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+       private readonly AppDbContext _context;
+       
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
