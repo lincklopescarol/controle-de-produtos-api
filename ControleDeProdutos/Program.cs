ee445e9fe56dda7c6b978dbda051487448ff5a5c
lincklopescarol <lincklopescarol@gmail.com>
2024-02-21 19:45:51 -0300

    feat: criação de CategoriasController e refatoração na classe de Produto

--- ControleDeProdutos/Program.cs
+++ ControleDeProdutos/Program.cs
@@ -1,5 +1,6 @@
 using ControleDeProdutos.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace ControleDeProdutos
 {
@@ -11,7 +12,8 @@ namespace ControleDeProdutos
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options => 
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
